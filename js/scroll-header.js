54b23e3f76f3db9d035aec7704f9a94847d931ff
Valentin T <92257425+ElFatFat@users.noreply.github.com>
2023-01-25 15:26:19 +0100

    Improved mobile header

--- js/scroll-header.js
+++ js/scroll-header.js
@@ -1,7 +1,15 @@
-window.addEventListener('scroll', function() {
+window.addEventListener("scroll", function () {
     if (window.scrollY > 1) {
-      document.querySelector('#header').classList.add('headerScroll');
+        document.querySelector("#header").classList.add("headerScroll");
     } else {
-      document.querySelector('#header').classList.remove('headerScroll');
+        document.querySelector("#header").classList.remove("headerScroll");
     }
-  });
+});
+
+function showHeader() {
+    document.querySelector("#header").classList.add("headerShow");
+}
+
+function hideHeader() {
+    document.querySelector("#header").classList.remove("headerShow");
+}
